54ceadc66c0ec2959ac80b049f7ddb3acb670290
Luiz Eduardo Garcia <60155983+luizeduardogarcia@users.noreply.github.com>
2020-01-31 16:50:21 -0300

    Create Format-NumberToBytes.ps1
    Function for friendly format a given number to string with the proper unit in B, Kb, Mb or Gb.

---
+++ function_Format-NumberToBytes/Format-NumberToBytes.ps1
@@ -0,0 +1,45 @@
+<#
+.Synopsis
+   Convert a given number out to Bytes, KBytes, MBytes and GBytes
+.DESCRIPTION
+   Convert a given number out to Bytes, KBytes, MBytes and GBytes, rounding it with 2 decimal digits, returning a string with the resumed unit with it
+.PARAMETER Number
+   The number to be formated
+.EXAMPLE
+   Format-NumberToBytes -Number 12345678
+.EXAMPLE
+   (123456789,456789123,789456123) | % { Format-NumberToBytes $_ }
+
+.TODO
+   Find a way to work in pipeline without the foreach loop
+#>
+function Format-NumberToBytes
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([string])]
+    Param
+    (
+        # Number to be formated
+        [Parameter(
+            Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true,
+            Position=0
+            )
+        ]
+        [long]
+        $Number
+    )
+
+    Process
+    {
+        switch ($true)
+        {
+            ($Number -gt 1073741823) { "$([math]::Round(($Number/1Gb),2)) Gb"; break }
+            ($Number -gt 1048575)    { "$([math]::Round(($Number/1Mb),2)) Mb"; break }
+            ($Number -gt 1023)       { "$([math]::Round(($Number/1Kb),2)) Kb"; break }
+            ($Number -le 1023)       { "$([math]::Round(($Number),2)) b" }
+        }
+    }
+}
+
