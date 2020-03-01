7cd174d368223ff48a1ecbf58c49595999e983e7
Luiz Eduardo Garcia <60155983+luizeduardogarcia@users.noreply.github.com>
2020-03-01 11:57:22 -0300

    Create Get-OS-Version-with-Powershell.ps1

---
+++ _adventures/Get-OS-Version-with-Powershell.ps1
@@ -0,0 +1,9 @@
+# Returns bellow are based on execution on Windows PS Console
+# Works in any Windows based OS version
+# Works with PS Core 6.0 (and up) at Unix/Linux distributions
+
+# To get Platform, ServicePack, Version, VersionString
+[System.Environment]::OSVersion
+
+# To get details, such as Version Major, Minor, Build, Revision
+[System.Environment]::OSVersion.Version
